a3a9bf9e865eb890442696209f0f1624e8b4cb95
PlatyPew <daryl.limkz@gmail.com>
2019-01-30 13:15:42 +0800

    Removed YES.

--- configs/i3/random_wallpaper.sh
+++ configs/i3/random_wallpaper.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 path='/root/Pictures/solarized_wallpaper'
 wallpaper=$(ls ${path} -1 | shuf -n 1)
-wallpaper=solarized-wallpaper-linuxmasterrace.png
 feh --bg-fill "${path}/${wallpaper}"
-sed -e '4d;6,8d' /root/.dotfiles/configs/i3/random_wallpaper.sh > /tmp/random_wallpaper.sh
-chmod +x /tmp/random_wallpaper.sh
-mv /tmp/random_wallpaper.sh /root/.dotfiles/configs/i3/random_wallpaper.sh
